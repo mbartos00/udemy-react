157068793f8896e8314b66f8fbf9ea270f65dc32
mbartos00 <maciekbartos8@gmail.com>
2022-12-17 15:20:10 +0100

    Add card wrapper component and styles

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import './ExpenseItem.css';
-
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 const ExpenseItem = ({ date, title, amount }) => {
-  const month = date.toLocaleString('en-US', { month: 'long' });
-  const day = date.toLocaleString('en-US', { day: '2-digit' });
-  const year = date.getFullYear();
-
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
+        <ExpenseDate date={date} />
       </div>
       <div className='expense-item__description'>
         <h2>{title}</h2>
-        <div className='expense-item__price'>{amount}</div>
+        <div className='expense-item__price'>${amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import Card from './Card';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
 const Expenses = ({ expenses }) => {
   return (
-    <div className='expenses'>
+    <Card className='expenses'>
       {expenses.map(({ id, title, amount, date }) => (
         <ExpenseItem key={id} title={title} amount={amount} date={date} />
       ))}
-    </div>
+    </Card>
   );
 };
 export default Expenses;
