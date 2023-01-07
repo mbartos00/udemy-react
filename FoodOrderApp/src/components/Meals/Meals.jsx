01cc761e2de10454c6cc4406fbeda6d2e53bbcb1
mbartos00 <maciekbartos8@gmail.com>
2023-01-07 12:51:02 +0100

    Add firebase and render data from it instead of dummy data

--- FoodOrderApp/src/components/Meals/Meals.jsx
+++ FoodOrderApp/src/components/Meals/Meals.jsx
@@ -2,55 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 import MealItem from './MealItem';
 import Card from '../UI/Card';
-
+import useMeals from '../hooks/useMeals';
 const StyledMain = styled.main`
   max-width: 80%;
   margin: 0 auto;
   margin-top: 3rem;
 `;
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
 const Meals = () => {
+  const { isLoading, error, meals } = useMeals();
   return (
     <StyledMain>
       <section>
         <Card>
           <ul>
-            {DUMMY_MEALS.map(({ id, name, description, price }) => (
-              <MealItem
-                key={id}
-                id={id}
-                name={name}
-                description={description}
-                price={price}
-              />
-            ))}
+            {error && (
+              <p style={{ color: '#000', textAlign: 'center' }}>{error}</p>
+            )}
+            {isLoading ? (
+              <p style={{ color: '#000', textAlign: 'center' }}>Loading...</p>
+            ) : (
+              meals.map(({ id, name, description, price }) => (
+                <MealItem
+                  key={id}
+                  id={id}
+                  name={name}
+                  description={description}
+                  price={price}
+                />
+              ))
+            )}
           </ul>
         </Card>
       </section>
